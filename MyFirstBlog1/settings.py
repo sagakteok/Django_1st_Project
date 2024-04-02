d00755b7bddea1c3f9ff6b4679a5e6c80ceff99c
sagakteok <maze77777@naver.com>
2024-04-02 13:45:31 +0900

    settings.py ALLOWED_HOSTS fixed

--- MyFirstBlog1/settings.py
+++ MyFirstBlog1/settings.py
@@ -23,10 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-)@_-u_(h_)o183bz4bq76nx24*y@(iopsz+2o*v-vw1hwx(d1n"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*', 'ShinYongBeom.pythonanywhere.com']
 
 # Application definition
 
